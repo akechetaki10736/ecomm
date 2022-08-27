b48403a14c1725b7edaa07377799829c886dea2e
kit <tingiao.ki@ttpsc.com>
2022-08-27 19:27:23 +0800

    Finish admin function.

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -29,7 +29,7 @@ router.post(
         const { id } = await usersRepo.create({ email, password });
         req.session.userId = id;
 
-        res.send('Account created');
+        res.redirect('/admin/products');
     });
 
 router.get('/signout', (req, res) => {
@@ -47,7 +47,7 @@ router.post('/signin', [ requireEmailExist, requireValidPasswordForUser ], handl
     const user = await usersRepo.getOneBy({ email });
 
     req.session.userId = user.id;
-    res.send('You are sign in.')
+    res.redirect('/admin/products');
 });
 
 module.exports = router;
