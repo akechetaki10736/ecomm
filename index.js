50d1a142e406f5aed81f75cbf797c4d6a9877dc2
kit <tingiao.ki@ttpsc.com>
2022-08-27 10:49:50 +0800

    Add css style.

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const authRouter = require('./routes/admin/auth');
 
 const app = express();
 
+// tell express server to look for a folder which is called "public" and expose it to oustside world.
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({
     // keys use to encrypt cookie, can put random string totally up to you.
